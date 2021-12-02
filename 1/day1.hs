84f153a0e299ee3313a405d400a711433f41294a
Meowyam <maryam@chilon.net>
2021-12-02 16:19:27 +0800

    replace counter with list:

--- 1/day1.hs
+++ 1/day1.hs
@@ -1,23 +1,20 @@
-import Data.List
 import System.IO
 import Control.Monad
 
 main = do
   input <- readFile "input.txt"
   let toInt = [ read i :: Integer | i <- words input]
-  let result = seeNext toInt 0
+  let result = seeNext toInt []
   print "day 1 part 1"
-  print result
+  print $ length result
   print "day 1 part 2"
-  let result2 = seeNext (windows toInt []) 0
-  print result2
+  let result2 = seeNext (windows toInt []) []
+  print $ length result2
 
-seeNext (x:y:xs) n | y > x = seeNext (y:xs) (n + 1)
-seeNext (x:y:xs) n = seeNext (y:xs) n
-seeNext _ n = n
+seeNext (x:y:xs) l
+  | y > x = seeNext (y:xs) $ l ++ [y:xs]
+  | otherwise = seeNext (y:xs) l
+seeNext _ l = l
 
 windows (x:y:z:xs) size = windows (y:z:xs) $ size ++ [x+y+z]
 windows _ size = size
-
-
-
